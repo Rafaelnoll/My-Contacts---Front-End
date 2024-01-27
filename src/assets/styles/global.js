ceed0de5882f84decfa928fd0045e133689f1ee2
Rafael Noll <rafanoll2004@gmail.com>
2024-01-27 17:54:15 -0300

    Styles: Addingo global styles

---
+++ src/assets/styles/global.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Sora', sans-serif;
+    }
+
+    body {
+      background-color: ${({ theme }) => theme.backgroundColor};
+      font-size: 16px;
+    }
+
+    button {
+      cursor: pointer;
+    }
+`;
+
+export default GlobalStyles;
