058cf71291748d5f696f2c0827d5856ea25ed74a
Rafael Noll <rafanoll2004@gmail.com>
2024-01-27 21:30:12 -0300

    Styles: Adding Contact Card component

--- src/components/ContactsList/styles.js
+++ src/components/ContactsList/styles.js
@@ -4,6 +4,28 @@ export const Container = styled.div`
   margin-top: 32px;
 `;
 
+export const ListContainer = styled.div`
+  margin-top: 16px;
+
+  header {
+    margin-bottom: 8px;
+
+    button {
+      display: flex;
+      align-items: center;
+
+      background-color: transparent;
+      border: none;
+
+      & span {
+        font-weight: bold;
+        margin-right: 8px;
+        color: ${({ theme }) => theme.colors.primary.main};
+      }
+    }
+  }
+`;
+
 export const Header = styled.header`
   display: flex;
   justify-content: space-between;
@@ -29,6 +51,60 @@ export const Header = styled.header`
     &:hover {
       color: #fff;
       background-color: ${({ theme }) => theme.colors.primary.main};
+    }
+  }
+`;
+
+export const Card = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  background-color: #fff;
+  border-radius: 4px;
+  padding: 16px;
+  box-shadow: 0px 4px 10px 0px #0000000A;
+
+  & + & {
+    margin-top: 16px;
+  }
+
+  .info{
+
+    span {
+      display: block;
+      color: ${({ theme }) => theme.colors.gray[200]};
+    }
+
+    .contact-name {
+      display: flex;
+      align-items: center;
+
+      strong {
+        color: ${({ theme }) => theme.colors.gray[900]};
+        margin-right: 8px;
+      }
+
+      small {
+        font-size: 12px;
+        color: ${({ theme }) => theme.colors.primary.main};
+        font-weight: bold;
+        text-transform: uppercase;
+        background-color: ${({ theme }) => theme.colors.primary.light};
+        border-radius: 4px;
+        padding: 4px 6px;
+      }
+    }
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+
+    button {
+      border: none;
+      background-color: transparent;
+      margin-left: 8px;
     }
   }
 `;
