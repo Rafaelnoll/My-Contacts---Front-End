3e1a19ab470af56426b4b4bcbdbe71100d4fb730
Rafael Noll <rafanoll2004@gmail.com>
2024-02-05 21:03:48 -0300

    Feature: Adding error validation in fields name and email of ContactForm

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -7,12 +7,15 @@ import Select from '../Select';
 import { Form } from './styles';
 import FormGroup from '../FormGroup';
 import { ButtonContainer } from '../FormGroup/styles';
+import isEmailValid from '../../utils/isEmailValid';
 
 function ContactForm({ buttonText = '', options = [], onConfirm }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [category, setCategory] = useState('');
+
+  const [errors, setErrors] = useState([]);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -25,14 +28,48 @@ function ContactForm({ buttonText = '', options = [], onConfirm }) {
     });
   }
 
+  function getFieldError(field) {
+    return errors.find((error) => error.field === field);
+  }
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setErrors((prevState) => ([
+        ...prevState,
+        { field: 'name', error: 'Nome é obrigatório' },
+      ]));
+    } else {
+      setErrors((prevState) => prevState.filter((error) => error.field !== 'name'));
+    }
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      const emailAlreadyExists = getFieldError('email');
+
+      if (emailAlreadyExists) return;
+
+      setErrors((prevState) => ([
+        ...prevState,
+        { fields: 'email', error: 'E-mail inválido' },
+      ]));
+    } else {
+      setErrors((prevState) => prevState.filter((error) => error.field !== 'email'));
+    }
+  }
+
   return (
     <Form onSubmit={handleSubmit}>
       <FormGroup>
-        <Input placeholder="Nome" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <Input placeholder="Nome" type="text" value={name} onChange={handleNameChange} />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="E-mail" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <Input placeholder="E-mail" type="email" value={email} onChange={handleEmailChange} />
       </FormGroup>
 
       <FormGroup>
